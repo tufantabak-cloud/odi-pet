2d3a91c8f26a98b83856ddc70a6fbc26b3cdcb7f
Tufan TABAK <tufantabak@cloud.com>
2026-05-04 19:29:21 +0300

    fix(ci): add webServer to playwright config to resolve connection refused error

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,4 +30,11 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
