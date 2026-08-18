4db86b8f2a8ee6110b8769a4cec6091afd7ac161
Tufan Tabak <tufan.tabak@gmail.com>
2026-08-18 13:04:51 +0300

    feat(monitoring): Sentry Next.js SDK entegrasyonu ve yapilandirmasi

---
+++ sentry.server.config.ts
@@ -0,0 +1,22 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://482cfc8ee615230b5663aa41aabf5821@o4511931128807424.ingest.de.sentry.io/4511931200241744",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
